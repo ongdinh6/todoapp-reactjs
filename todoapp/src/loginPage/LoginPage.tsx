11834a943bafd09384601407494d1b6197a4ed2a
ongdinh6 <ongdinh6@gmail.com>
2023-11-18 23:20:03 +0700

    11182023|Remove no-inline-styles hints and handle login feature

--- todoapp/src/loginPage/LoginPage.tsx
+++ todoapp/src/loginPage/LoginPage.tsx
@@ -1,19 +1,61 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import loginService from "./service/loginService";
+import { useState } from "react";
+
+const LoginService = loginService;
+const enum FormFields {
+    UNAME = "Username",
+    PWD = "Password"
+};
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
+    const [uname, setUname] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = (username: string, pwd: string) => {
+        LoginService.login(username, pwd)
+            .then((response: any) => {
+                console.log("Login Success with the user details: ", response);
+                // Todo: navigate to the previous page
+                navigate("/");
+            })
+            .catch((error: any) => {
+                console.error("Login Failed with details: ", error)
+                setErrorMessage(error.errorMessage);
+            });
+    }
+
+
+    const onFieldChange = (e: any, type: FormFields) => {
+        switch (type) {
+            case FormFields.UNAME:
+                setUname(e.target.value);
+                break;
+            case FormFields.PWD:
+                setPwd(e.target.value);
+                break;
+            default:
+                throw Error("Cannot handle onChange the unknown type!");
+        }
+    }
+
 
     return <div>
+        <small style={{ color: "red" }}>{errorMessage}</small>
         <div>
             <label>Username:</label>
-            <input title="Type username here!" type="text" />
+            <input title="Type username here!" type="text" onChange={(e) => onFieldChange(e, FormFields.UNAME)} />
         </div>
         <div>
             <label>Password:</label>
-            <input title="Type password here!" type="password" />
+            <input title="Type password here!" type="password" onChange={(e) => onFieldChange(e, FormFields.PWD)} />
         </div>
-        <button>Submit</button>
+        <button onClick={() => handleSubmit(uname, pwd)}>Submit</button>
         <Link to="/">Go to Home page</Link>
-    </div>
+    </div >
 }
 
 export default LoginPage;
---
+++ todoapp/src/loginPage/service/loginService.ts
@@ -0,0 +1,18 @@
+class LoginService {
+
+    login(username: string, pwd: string): Promise<any> {
+        if (username === "user" && pwd === "password") {
+            return Promise.resolve({
+                status: 200,
+                isAuthenticated: true,
+                token: "accesstokenisenabled"
+            });
+        }
+
+        return Promise.reject({errorMessage: "Your username or password is incorrect!"});
+    };
+
+}
+
+const loginService = new LoginService();
+export default loginService;
